4331b205dcec493fa74d6174e6935d11cb83ea82
Yohan Gomez <yohan.gz@hotmail.com>
2017-12-12 15:29:51 +0530

    Add routing support for frontend.

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
+import { HashRouter, Route, Link } from 'react-router-dom';
 
 import reactLogo from './images/react.svg';
 import playLogo from './images/play.svg';
 import javaLogo from './images/java.webp';
 
 import './App.css';
+
+const Tech = ({ match }) => {
+  return <div>Current Route: {match.params.tech}</div>
+};
+
 
 class App extends Component {
   constructor(props) {
@@ -23,20 +29,31 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <h1>Welcome to {this.state.title}!</h1>
-        <nav>
-          <a>
-            <img width="400" height="400" src={javaLogo} alt="Java Logo" />
-          </a>
-          <a>
-            <img width="400" height="400" src={playLogo} alt="Play Framework Logo" />
-          </a>
-          <a>
-            <img width="400" height="400" src={reactLogo} className="App-logo" alt="React Logo"/>
-          </a>
-        </nav>
-      </div>
+      <HashRouter>
+        <div className="App">
+          <h1>Welcome to {this.state.title}!</h1>
+          <nav>
+            <Link to="java" >
+              <img width="400" height="400" src={javaLogo} alt="Java Logo" />
+            </Link>
+            <Link to="play" >
+              <img width="400" height="400" src={playLogo} alt="Play Framework Logo" />
+            </Link>
+            <Link to="react" >
+              <img width="400" height="400" src={reactLogo} className="App-logo" alt="React Logo"/>
+            </Link>
+          </nav>
+          <Route path="/:tech" component={Tech} />
+          <div>
+            <h2>Check out the project on GitHub for more information</h2>
+            <h3>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/yohangz/java-play-react-seed">
+                java-play-react-seed
+              </a>
+            </h3>
+          </div>
+        </div>
+      </HashRouter>
     );
   }
 }
