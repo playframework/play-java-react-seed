40f20a55525e4e2881fb60611299dc4a52f615ff
gayana <gayan.kavindra06@gmail.com>
2017-12-18 22:21:01 +0530

    Update frontend unit tests. Update package.json

--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -1,8 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import App from './App';
+import Client from "./Client";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+jest.mock('../src/Client');
+
+describe('App tests', () => {
+  let app;
+  const response = {
+    content: 'Java Play React Seed'
+  };
+
+  beforeEach(() => {
+    app = Enzyme.shallow(
+      <App/>
+    );
+  });
+
+  afterEach(() => {
+    Client.getSummary.mockClear();
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should set the state property `title`', () => {
+    const invocationArgs = Client.getSummary.mock.calls[0];
+    const cb = invocationArgs[0];
+    cb(response);
+    app.update();
+
+    expect(
+      app.state().title
+    ).toEqual(response.content);
+  });
 });
