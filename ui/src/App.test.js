214f98dcd3662f60a99db35e812ba4bcde7bb788
gayana <gayan.kavindra06@gmail.com>
2017-12-19 19:54:42 +0530

    Update unit tests and remove dependencies.

--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -1,31 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import renderer from 'react-test-renderer';
 
 import App from './App';
 import Client from "./Client";
 
-Enzyme.configure({ adapter: new Adapter() });
-
 jest.mock('../src/Client');
 
 describe('App tests', () => {
-  let app;
-  const response = {
-    content: 'Java Play React Seed'
-  };
-
-  beforeEach(() => {
-    app = Enzyme.shallow(
-      <App/>
-    );
-  });
-
-  afterEach(() => {
-    Client.getSummary.mockClear();
-  });
 
   it('renders without crashing', () => {
     const div = document.createElement('div');
@@ -33,14 +14,7 @@ describe('App tests', () => {
     ReactDOM.unmountComponentAtNode(div);
   });
 
-  it('should set the state property `title`', () => {
-    const invocationArgs = Client.getSummary.mock.calls[0];
-    const cb = invocationArgs[0];
-    cb(response);
-    app.update();
-
-    expect(
-      app.state().title
-    ).toEqual(response.content);
+  it('getSummary function should called exactly once', () => {
+    expect(Client.getSummary.mock.calls.length).toBe(1);
   });
 });
