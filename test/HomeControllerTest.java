8ebd1eb6e6071aacad59c4dbda511dd7d6810f26
gayana <gayan.kavindra06@gmail.com>
2018-01-31 19:52:24 +0530

    Update java controller unit tests

--- test/HomeControllerTest.java
+++ test/HomeControllerTest.java
@@ -1,16 +1,41 @@
 import controllers.HomeController;
 import org.junit.Test;
+import play.Application;
+import play.inject.guice.GuiceApplicationBuilder;
+import play.mvc.Http;
 import play.mvc.Result;
+import play.test.WithApplication;
 
 import static org.junit.Assert.assertEquals;
 import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.GET;
+import static play.test.Helpers.contentAsString;
+import static play.test.Helpers.route;
 
-public class HomeControllerTest {
+public class HomeControllerTest extends WithApplication {
+
+    @Override
+    protected Application provideApplication() {
+        return new GuiceApplicationBuilder().build();
+    }
 
     @Test
-    public void testAppSummary() {
+    public void test_app_summary_from_a_new_instance() {
         Result result = new HomeController().appSummary();
         assertEquals(OK, result.status());
         assertEquals("application/json", result.contentType().get());
+        assertEquals(contentAsString(result), "{\"content\":\"Java Play React Seed\"}");
+    }
+
+    @Test
+    public void test_app_summary_from_route() {
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(GET)
+                .uri("/api/summary");
+
+        Result result = route(app, request);
+        assertEquals(OK, result.status());
+        assertEquals("application/json", result.contentType().get());
+        assertEquals(contentAsString(result), "{\"content\":\"Java Play React Seed\"}");
     }
 }
